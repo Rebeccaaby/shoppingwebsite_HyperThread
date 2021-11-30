9c2b77298abf356983d07bc826766f26126058f1
RebeccaAbraham21 <rba5216@psu.edu>
2021-11-30 14:06:27 -0500

    Backend changes

--- backend/Product.js
+++ backend/Product.js
@@ -5,10 +5,10 @@ var productSchema = new mongoose.Schema({
         description: {type: String, required: true},
         image: {type: String, required: true},
         categories: {type: Array},
-        size: {type: String},
-        color: {type: String},
+        size: {type: Array},
+        color: {type: Array},
         price: {type: Number, required: true},
-
+        inStock: { type: Boolean, default:true},
     },
     {timestamps: true}
 );
